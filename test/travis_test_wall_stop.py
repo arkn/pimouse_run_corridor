fc0ba53ae8b01e083c51a42f79cd58e376c51c9b
arkn <ken.arai.jp@gmail.com>
2020-01-05 02:12:42 +0000

    fixed

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -20,14 +20,14 @@ class WallStopTest(unittest.TestCase):
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) # total = 600
         self.assertTrue(left == 0 and right == 0, "can't stop")
-        
+
         left, right = self.est_and_get(400,0,0,99) # total = 499
         self.assertTrue(left != 0 and right != 0, "can't move again")
 
         left, right = self.set_and_get(150,0,200,150) # total = 500
         self.assertTrue(left == 0 and right == 0, "can't stop")
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_wall_stop')
     rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop',WallStopTest)
