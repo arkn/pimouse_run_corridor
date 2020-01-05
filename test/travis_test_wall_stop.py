938096444a2909f439d0374a9bd591b7ad123313
arkn <ken.arai.jp@gmail.com>
2020-01-05 02:16:53 +0000

    fixed

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -21,7 +21,7 @@ class WallStopTest(unittest.TestCase):
         left, right = self.set_and_get(400,100,100,0) # total = 600
         self.assertTrue(left == 0 and right == 0, "can't stop")
 
-        left, right = self.est_and_get(400,0,0,99) # total = 499
+        left, right = self.set_and_get(400,0,0,99) # total = 499
         self.assertTrue(left != 0 and right != 0, "can't move again")
 
         left, right = self.set_and_get(150,0,200,150) # total = 500
