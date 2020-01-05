b6c0b22cc9ff118c15462428387893e4b622d4fa
arkn <ken.arai.jp@gmail.com>
2020-01-05 02:03:44 +0000

    fixed

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import rospy,copy
+import rospy, copy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
@@ -7,8 +7,9 @@ from pimouse_ros.msg import LightSensorValues
 class WallStop():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
+
         self.sensor_value = LightSensorValues()
-        rospy.Subscriber('/lightseonsors',LightSensorValues,self.callback)
+        rospy.Subscriber('/lightsensors',LightSensorValues,self.callback)
 
     def callback(self,messages):
         self.sensor_values = messages
